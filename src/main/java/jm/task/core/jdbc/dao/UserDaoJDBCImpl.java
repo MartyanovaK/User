f528e48b3212655cb35a102fe298217e677df61e
MartyanovaK <132018032+MartyanovaK@users.noreply.github.com>
2023-06-05 17:49:01 +0300

    Update UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -20,61 +20,65 @@ public class UserDaoJDBCImpl implements UserDao {
 
 
    public void createUsersTable() {
-//        try(Statement statement = Util.getConnection().createStatement()) {
-//            statement.executeUpdate("create table User(id int primary key auto_icrement, name varchar, lastName varchar, age int)");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//       }
+        try(Statement statement = Util.getConnection().createStatement()) {
+            statement.executeUpdate("create table User(id int primary key auto_icrement, name varchar, lastName varchar, age int)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+       }
    }
 
    public void dropUsersTable() {
-//        try(Statement statement = connection.createStatement()) {
-//            statement.executeUpdate("drop table User");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+       try(Statement statement = connection.createStatement()) {
+           statement.executeUpdate("drop table User");
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate("create table User(id int primary key auto_icrement, name varchar, lastName varchar, age int)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-//        try(PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM User WHERE id = ?")) {
-//            preparedStatement.setLong(1, id);
-//            preparedStatement.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+       try(PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM User WHERE id = ?")) {
+           preparedStatement.setLong(1, id);
+           preparedStatement.executeUpdate();
+
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
     }
 
    public List<User> getAllUsers() {
-//        List<User> userList = new ArrayList<>();
-//        try(Statement statement = connection.createStatement()) {
-//            ResultSet resultSet = statement.executeQuery("SELECT ID, NAME, LASTNAME, AGE FROM User");
-//            while (resultSet.next()) {
-//                User user = new User();
-//                user.setId(resultSet.getLong("ID"));
-//                user.setName(resultSet.getString("NAME"));
-//                user.setLastName(resultSet.getString("LASTNAME"));
-//                user.setAge(resultSet.getByte("AGE"));
-//
-//                userList.add(user);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return userList;
-       return null;
+       List<User> userList = new ArrayList<>();
+       try(Statement statement = connection.createStatement()) {
+           ResultSet resultSet = statement.executeQuery("SELECT ID, NAME, LASTNAME, AGE FROM User");
+           while (resultSet.next()) {
+               User user = new User();
+               user.setId(resultSet.getLong("ID"));
+               user.setName(resultSet.getString("NAME"));
+               user.setLastName(resultSet.getString("LASTNAME"));
+               user.setAge(resultSet.getByte("AGE"));
+
+               userList.add(user);
+           }
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+       return userList;
+       
    }
-//
+
    public void cleanUsersTable() {
-//        try (Statement statement = connection.createStatement()) {
-//            statement.executeUpdate("TRUNCATE TABLE User");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+       try (Statement statement = connection.createStatement()) {
+           statement.executeUpdate("TRUNCATE TABLE User");
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
     }
 }
